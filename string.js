5519a388aa4e2e80ded9c34259d10b9f73429144
lorena <lorenaralmeida06@gmail.com>
2026-06-19 16:08:48 -0300

    vetor vazio
    foram adicionados os itens que ficaram no tabuleiro, porém sem a imagens inicialmente

---
+++ string.js
@@ -0,0 +1,28 @@
+function jogarBN(){
+let itens = [];
+// criamos um vetor vazio
+for(let i=0;i<10;i++){
+    itens.push("b1");
+}
+// adicionamos todos os elementos que ficaram na matriz
+for(let i=0;i<10;i++){
+    itens.push("b2");
+}
+
+for(let i=0;i<10;i++){
+    itens.push("b3");
+}
+
+for(let i=0;i<35;i++){
+    itens.push("bm");
+}
+
+for(let i=0;i<35;i++){
+    itens.push("H2O");
+}
+
+
+}
+
+
+
