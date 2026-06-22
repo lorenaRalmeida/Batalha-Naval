b509e85d09fdbf5ea3131a845139a9284b3a24b3
Davi Silva <davisousa33301@gmail.com>
2026-06-22 18:58:03 -0300

    arrumando as imagens

--- script/script-jogo.js
+++ script/script-jogo.js
@@ -73,7 +73,7 @@ for(let i = 0; i < 10; i++){
 
             celula.dataset.aberta = "sim";
 
-            celula.innerHTML =`<img src="..img/jogo/${imgCorrespondente}.png" width="50" height="50">`;
+            celula.innerHTML =`<img src="../img/jogo/${imgCorrespondente}.png" width="50" height="50">`;
 
             if(imgCorrespondente === "barco1"){
                 pontos += 10;
